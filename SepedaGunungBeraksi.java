cec7f1b11b1c17309bd7a5bc1364bd0b23be1b86
aduyyy <wahyusutikno0702@gmail.com>
2024-03-27 14:57:20 +0700

    praktikum 1

---
+++ SepedaGunungBeraksi.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pewarisan;
+
+/**
+ *
+ * @author usER
+ */
+public class SepedaGunungBeraksi {
+    public static void main (String[] args){
+     
+    SepedaGunung sepedaku = new SepedaGunung() ;
+    sepedaku.tambahKecepatan(10);
+    sepedaku.ubahGir(2);
+    sepedaku.setSadel(20);        
+    }
+}
---
+++ latihanVar.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author usER
+ */
+public class latihanVar {
+      public static void main(String[] args) {
+        // code aplication logic here
+        int JariJari;
+        double Keliling;
+        final double PI=3.14;
+        
+        JariJari =30;
+        Keliling = 2*PI*JariJari;
+        System.out.println ("Keliling Limgkaran = "+ Keliling);
+        System.out.println ("==============================");
+    }
+}
